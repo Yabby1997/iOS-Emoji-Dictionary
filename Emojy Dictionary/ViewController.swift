894979f326cbe9e6238f0ecc1306d2c1d1efc937
yabby1997 <yabby1997@gmail.com>
2021-02-12 21:43:43 +0900

    Initial Commit

---
+++ Emojy Dictionary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Emojy Dictionary
+//
+//  Created by yabby on 2021/02/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
